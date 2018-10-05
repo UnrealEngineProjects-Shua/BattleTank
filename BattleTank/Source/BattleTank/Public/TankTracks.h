e30a6943c1f64604f16b12fe8eb06388308dd591
Joshua Mathews <math.joshua11@gmail.com>
2018-10-05 00:16:13 -0600

    Begining tank movement
    Moves with gravity turned off.
    Need to overcome friction still.

--- BattleTank/Source/BattleTank/Public/TankTracks.h
+++ BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -15,9 +15,12 @@ class BATTLETANK_API UTankTracks : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
+	// Set throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	// Max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 40000 * 10; // Assume 10 ton tank and 1g acceleration
 	
 };
