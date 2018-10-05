d8055dca1119a69693ac028a0a15442cd70c7049
Joshua Mathews <math.joshua11@gmail.com>
2018-10-05 11:34:18 -0600

    Tank begins moving
    Changed tank physics
    Changed tank collisions
    Created new empty class for tank movement controller

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -26,7 +26,7 @@ void ATankAIController::Tick(float DeltaTime)
 		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
 		//Fire if ready
-		ControlledTank->Fire();
+		//ControlledTank->Fire();
 	}
 
 	/*if (!PlayerTank)
